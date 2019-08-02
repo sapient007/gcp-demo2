57b9aaae08f99f94e61a8272f53c0fb8ab2230e3
stefanifournier <stefani.fournier@gmail.com>
2019-08-02 19:22:51 +0000

    Updates to demo notebook

--- friday_training/train.py
+++ friday_training/train.py
@@ -2,6 +2,7 @@ import datetime
 import xgboost as xgb
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 import subprocess
 from google.cloud import storage
 import pandas as pd
@@ -35,9 +36,9 @@ numeric_features = train.select_dtypes(include=[np.number])
 numeric_features.dtypes
 
 
-# categorical columns
-categorical_columns = ["Product_ID", "Gender", "Age", "Occupation", "City_Category", "Stay_In_Current_City_Years",
-                       "Marital_Status", "Product_Category_1", "Product_Category_2", "Product_Category_3"]
+# categorical columns to convert
+categorical_columns = ["Gender", "Age", "Occupation", "City_Category", "Stay_In_Current_City_Years",
+                       "Marital_Status", "Product_Category_1"]
 
 
 # Join Train and Test Dataset so it can be cleaned all at once
@@ -46,44 +47,32 @@ test['source']='test'
 
 data = pd.concat([train,test], ignore_index = True, sort = False)
 
-#imput the value -2 for NaN since NaN most likely means that the person did not buy products from these categories
-data["Product_Category_2"]= \
-data["Product_Category_2"].fillna(-2.0).astype("float")
-
-data["Product_Category_3"]= \
-data["Product_Category_3"].fillna(-2.0).astype("float")
-
 #Get index of all columns with product_category_1 equal 19 or 20 from train and remove since not populated
 condition = data.index[(data.Product_Category_1.isin([19,20])) & (data.source == "train")]
 data = data.drop(condition)
 
+# define example
+#community_area = [num for num in range(78)]
+# data = array(data)
+#print(community_area)
+# one hot encode
 
 # convert categorical data to to numerical values.
 # convert data in categorical columns to numerical values
-encoders = {col:LabelEncoder() for col in categorical_columns}
+"""encoders = {col:LabelEncoder() for col in categorical_columns}
 for col in categorical_columns:
     data[col] = encoders[col].fit_transform(data[col])
+    #data[col] = to_categorical(data[col])
+    #print(data[col])
+"""
+data = pd.get_dummies(data, columns=categorical_columns, drop_first=False)
+#print(data)
 
-#create count features of each user
-def getCountVar(compute_df, count_df, var_name):
-    grouped_df = count_df.groupby(var_name)
-    count_dict = {}
-    for name, group in grouped_df:
-        count_dict[name] = group.shape[0]
-
-    count_list = []
-    for index, row in compute_df.iterrows():
-        name = row[var_name]
-        count_list.append(count_dict.get(name, 0))
-    return count_list
-
-data["Age_Count"]  =getCountVar(data, data, "Age")
-data["Occupation_Count"]  =getCountVar(data, data, "Occupation")
-data["Product_Category_1_Count"]  =getCountVar(data, data,"Product_Category_1")
-data["Product_Category_2_Count"]  =getCountVar(data, data, "Product_Category_2")
-data["Product_Category_3_Count"]  =getCountVar(data, data,"Product_Category_3")
-data["Product_ID_Count"]  =getCountVar(data, data, "Product_ID")
-
+totalitem = data['User_ID'].value_counts().sort_index()
+totalpurchase = data.groupby('User_ID').sum()['Purchase']
+tot = pd.concat([totalitem, totalpurchase], axis =1, keys = ['Total_products', 'Total_purchase'])
+data = pd.merge(data, tot, left_on = 'User_ID', right_index = True)
+    
 #Divide into test and train
 train = data.loc[data['source']=="train"]
 test = data.loc[data['source']=="test"]
@@ -92,12 +81,12 @@ test = data.loc[data['source']=="test"]
 test.drop(['source'],axis=1,inplace=True)
 train.drop(['source'],axis=1,inplace=True)
 
-# remove column we are trying to predict ('Purchase') from features list
-train_features = train.drop('Purchase', axis=1)
-test_features = test.drop('Purchase', axis=1)
+# remove column we are trying to predict ('Purchase') from features list and also removed product category 2 and 3 due to missing values
+train_features = train.drop(['Purchase', 'Product_Category_2', 'Product_Category_3', 'Product_ID', 'User_ID'], axis=1)
+test_features = test.drop(['Purchase', 'Product_Category_2', 'Product_Category_3', 'Product_ID', 'User_ID'], axis=1)
 # create training labels list
-train_labels = (train['Purchase'])
-test_labels = test['Purchase']
+train_labels = train[['Purchase']]
+test_labels = test[['Purchase']]
 
 # load data into DMatrix object
 dtrain = xgb.DMatrix(train_features, train_labels)
@@ -116,4 +105,13 @@ blob = bucket.blob(model)
 blob.upload_from_filename(model)
 
 
+#tree based learner for onehotencode
+from sklearn.metrics import mean_squared_error
+xg_reg = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 200)
 
+xg_reg.fit(train_features,train_labels)
+
+preds = xg_reg.predict(train_features)
+rmse = np.sqrt(mean_squared_error(train_labels, preds))
+print("RMSE: %f" % (rmse))
